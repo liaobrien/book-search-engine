b2505f226ea728e9b95651172539bed16d6dcdd9
Lia OBrien <liaobrien123@gmail.com>
2021-11-11 20:00:51 -0500

    added login to query, started mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -18,7 +18,39 @@ const resolvers = {
             },
             books: async () => {
                   return Book.find({});
-            }
+            },
+            login: async (parent, { email, password }) => {
+
+                  const user = await User.findOne({ email });
+
+                  if (!user) {
+                        throw new AuthenticationError('No user found with this email address');
+                  }
+
+                  const correctPw = await user.isCorrectPassword(password);
+
+                  if (!correctPw) {
+                        throw new AuthenticationError('Incorrect credentials');
+                  }
+
+                  const token = signToken(user);
+
+                  return { token, user };
+            },
       },
 
+      Mutation: {
+            createUser: async (parent, { username, email, password }) => {
+                  // First we create the user
+                  const user = await User.create({ username, email, password });
+                  // To reduce friction for the user, we immediately sign a JSON Web Token and log the user in after they are created
+                  const token = signToken(user);
+                  // Return an `Auth` object that consists of the signed token and user's information
+                  return { token, user };
+            },
+
+            saveBook: async (parent,) => {
+
+            },
+      }
 }
