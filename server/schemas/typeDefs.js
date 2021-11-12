b99966081b86d1fd29e631f3a36364af90768d95
Lia OBrien <liaobrien123@gmail.com>
2021-11-11 19:00:06 -0500

    filled out typeDefs.js, may need to edit

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,44 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+      type User {
+            _id: ID
+            username: String
+            email: String
+            bookCount: Number
+            savedBooks: [Book]
+      }
+
+      type Book {
+            bookId: String
+            authors: [String]
+            description: String
+            title: String
+            image: String
+            link: String
+      }
+
+      type Auth {
+            token: ID!
+            user: User
+      }
+
+      type Query {
+            users: [User]
+            user(username: String!): User
+            books(username: String): [Book]
+            book(bookId: ID!): Book
+      }
+
+      type Mutation {
+            addUser(username: String!, email: String!, password: String!): Auth
+            login(email: String!, password: String!): Auth
+            addBook(
+                  authors: [String], 
+                  description: [String], 
+                  bookId: String, 
+                  link: String, 
+                  title: String
+            ): Book
+      }
+`;
